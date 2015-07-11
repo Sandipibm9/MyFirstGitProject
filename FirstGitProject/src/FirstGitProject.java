99233ecfc7ef5ad2541b157a33cb2865d9f724d8
Sandipibm9 <sandipibm9@gmail.com>
2015-07-12 01:18:56 +0530

    First Git

---
+++ FirstGitProject/src/FirstGitProject.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class FirstGitProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("let's get started");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http:\\www.github.com");
+	}
+
+}
